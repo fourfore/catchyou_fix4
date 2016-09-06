0f47616a2ed98fdb896e897a256d7d085f8f6bb2
chalat@eoss-th.com <053988120a>
2016-09-06 02:19:59 -0700

    Login button

--- app/src/main/java/com/eoss/application/catchme_fix4/activity/LoginActivity.java
+++ app/src/main/java/com/eoss/application/catchme_fix4/activity/LoginActivity.java
@@ -1,12 +1,15 @@
 package com.eoss.application.catchme_fix4.activity;
 
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+
 import com.eoss.application.catchme_fix4.R;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
@@ -15,18 +18,18 @@ import com.parse.ParseUser;
 import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
-
-
+    ProgressDialog progress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_login);
-
     }
 
 
     public void onClickLogin(View view){
+        progress = ProgressDialog.show(this, "Now loading",
+                "Please wait for a while.", true);
         ParseFacebookUtils.logInWithReadPermissionsInBackground(this, Arrays.asList("email", "user_photos", "public_profile", "user_friends")
                 ,new LogInCallback() {
                     ParseUser user;
@@ -34,12 +37,14 @@ public class LoginActivity extends AppCompatActivity {
                     public void done(ParseUser user, ParseException err) {
                         if (user == null) {
                             Log.d("parse-->","cancel");
+                            progress.dismiss();
                         } else if (user.isNew()) {
                             Log.d("parse-->","User Already logged up through facebook! new");
                             user.getCurrentUser();
                             if(user != null){
                                 Intent intent = new Intent(LoginActivity.this,AppActivity.class);
                                 startActivity(intent);
+                                progress.dismiss();
                                 finish();
                             }
                         } else {
@@ -49,6 +54,7 @@ public class LoginActivity extends AppCompatActivity {
                                 Log.d("parse-->","Register");
                                 Intent intent = new Intent(LoginActivity.this,AppActivity.class);
                                 startActivity(intent);
+                                progress.dismiss();
                                 finish();
                             }
 
